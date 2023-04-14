873465d86cf176bf0b4b5973f5bb6c56c32850b1
River Ferguson <riverferguson@gmail.com>
2023-04-14 09:32:11 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('#create-task-form').addEventListener("submit", (e) => {
+    e.preventDefault()
+   createNewTask()
+   e.target.reset()
+  })
+//});
+
+const createNewTask = () => {
+  
+  const btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click', handleDelete)
+  const newTaskDescription = document.getElementById("new-task-description")
+  const newTask = document.createElement("li")
+  newTask.innerText = newTaskDescription.value
+  tasks.append(newTask)
+  newTask.append(btn)
+}
+
+const handleDelete = (e) => {
+ e.target.parentNode.remove()
+}
